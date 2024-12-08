24593d289b180726b45de080bd2489bcd46192ac
Briac PANNETIER <briac.pannetier.02@gmail.com>
2024-12-08 12:18:21 +0100

    fix: correct user route handling in backend

--- frontend/src/components/UserDash.js
+++ frontend/src/components/UserDash.js
@@ -11,12 +11,23 @@ const UserDash = ({ user, setUser }) => {
     const changePass = () => {
         window.location.href = '/reset-password-logged';
     };
+    const deleteAccount = async () => {
+        await axiosInstance.post('/user/delete-account-req', { withCredentials: true })
+            .then((res) => {
+                console.log(res);
+                setUser(null);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
     return (
         <div>
             <h1>Welcome, {user.username}!</h1>
             <p>Email: {user.email}</p>
             <button onClick={changeMail}>Change Email</button>
             <button onClick={changePass}>Reset Password</button>
+            <button onClick={deleteAccount}>Delete Account</button>
             <p>Role: {user.role}</p>
             {/* Use the LogoutButton component */}
             <LogoutButton setUser={setUser} />
