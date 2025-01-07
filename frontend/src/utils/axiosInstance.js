94d10edd1585922ee8d8ce618aed084e160269da
Briac PANNETIER <briac.pannetier.02@gmail.com>
2025-01-07 21:32:51 +0100

    feat : changed url for deployment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'https://app-sec-app-server-18blk3li4-briacs-projects-8dadbe9b.vercel.app/api', // The base URL for your API requests (change this if needed)
     withCredentials: true, // Ensures that cookies are included in the request (important for JWT in cookies)
+
 });
 
 export default axiosInstance;
