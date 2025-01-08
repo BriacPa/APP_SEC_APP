d3071ca910347867b2f1d8ca8b27ba107fc96dd0
Briac PANNETIER <briac.pannetier.02@gmail.com>
2025-01-08 12:54:34 +0100

    fix: update Axios base URL for API requests

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default settings
 const axiosInstance = axios.create({
-    baseURL: 'https://app-sec-app-server-hq6vprfos-briacs-projects-8dadbe9b.vercel.app/api', // The base URL for your API requests (change this if needed)
+    baseURL: 'https://app-sec-app-server.vercel.app/api', // The base URL for your API requests (change this if needed)
     withCredentials: true, // Ensures that cookies are included in the request (important for JWT in cookies)
 
 });
